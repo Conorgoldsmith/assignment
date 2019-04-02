78d466407ab85250a6ff1f340ef9bb6b683c422d
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-02 23:25:51 +0000

    added alphabet array and convert letter input to decimal

--- assignment.c
+++ assignment.c
@@ -2,8 +2,16 @@
 
 int main()
 {
-    int i;
-    scanf("%d", &i);
-    printf("%d", i);
-
+    int k; //degree of rotation
+    int alphabet[26];
+    for (int counter=1 ; counter <= 26 ; counter++)
+    {
+        alphabet[counter] = counter;
+    }
+    char inputLetter;
+    int inputLetterInteger;
+    scanf("%c", &inputLetter);
+    inputLetter=inputLetter-96;
+    inputLetterInteger=inputLetter;
+    printf("%d\n", alphabet[inputLetterInteger]);
 }
