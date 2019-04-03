e2ff35d1ae8f1a3eefe0ab503bac3ac366f6c7f6
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-03 00:50:33 +0000

    added test file and attempted to integrate string

--- assignment.c
+++ assignment.c
@@ -8,14 +8,37 @@ int main()
     {
         alphabet[counter] = counter;
     }
-    char inputLetter;
-    int inputLetterInteger;
-    scanf("%c", &inputLetter);
-    inputLetter=inputLetter-96;
-    inputLetterInteger=inputLetter;
-    printf("%d\n", alphabet[inputLetterInteger]);
-    
-    int newLetter;
-    newLetter=((inputLetter+k) % 26);
-    printf("%d", newLetter);
+    //char inputLetter;
+    char inputString[256];
+    scanf("%s", inputString);
+    //int inputLetterInteger;
+    //scanf("%c", &inputLetter);
+    //inputLetter=inputLetter-96;
+    inputString[256]=inputString[256]-96;
+    //inputLetterInteger=inputLetter;
+    //printf("%d\n", alphabet[inputLetterInteger]);
+    //char newLetter;
+    //int newLetterInteger;
+    char newInputString[256];
+    newInputString=((inputString)-96)+k);
+    //newLetterInteger=((inputLetter+k) % 26);
+    //newLetter=newLetterInteger+96;
+    //printf("%c", newLetter);
+    printf("%s", newInputString);
 }
+
+//#include <stdio.h>
+//int main() 
+//{
+  //  char string[] = "this is a string";
+    //int n;
+    //for(n=0 ; n<15 ; n++)
+    //{
+    //    string[n]=string[n]+1;
+    //}
+    //for(int i=0 ; i< 15 ; i++)
+    //{
+    //    printf("%c", string[i]);
+    //}
+
+//;}
