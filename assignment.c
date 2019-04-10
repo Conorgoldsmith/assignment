8db61d8aefd6d2b1b670b4e65022f540e87aa92f
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-10 00:50:43 +0000

    completed the encryption of rotation cypher

--- assignment.c
+++ assignment.c
@@ -1,18 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
 int main()
 {
 FILE *inputtext;
 char inputletters[256];
 char ch;
-int n=1;
+int k=18; //degree of rotation
+int inputletterint;
+char newLetter;
+int newLetterInteger;
 inputtext=fopen("input.txt", "r");
 
+
    while((ch = fgetc(inputtext)) != EOF)
+        for (int n=1 ; n<2 ; n++)
+        {
+    inputletterint=ch-96;
+    newLetterInteger=((inputletterint+k));
+    newLetter=newLetterInteger+96;
+    inputletters[n]=newLetter;
+    if(inputletters[1]<123 || inputletters[1]>96 || inputletters[1]==32){
+        if (inputletters[1]>122){
+            inputletters[1]=inputletters[1]-26;
+        }
+        if (inputletters[1]<97){
+            inputletters[1]=inputletters[1]+26;
+        }
+        if (inputletters[1]==76){
+            inputletters[1]=32;
+        }
+    }
+    if (inputletters[1]>123 || inputletters[1]<96){
+        inputletters[1]=ch;
+    }
 
-
-        printf("%c", ch);
-
+    printf("%c", inputletters[1]);
+    }
+    
     fclose(inputtext);
 }
 
