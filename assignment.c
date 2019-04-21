f4a4cba97cf1d9a1505351df1b0522ef5807c00b
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-21 06:55:35 +0000

    finished making funcyion

--- assignment.c
+++ assignment.c
@@ -1,71 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-char subCyph(char inputLetter, char location);
+int k1;
+char subCyph(char inputLetter);
 
 int main()
 {
     printf("Welcome, choose either:\n1.Rotation Cypher Encryption\n2.Rotation Cypher Decryption\n3.Substitution Cypher Encryption\n4.Substitution Cypher Decryption\n\n");
-    
-FILE *inputtext;
-static char ch;
-static int k=18; //degree of rotation
-int inputletterint;
-char newLetter;
-int newLetterInteger;
-inputtext=fopen("input.txt", "r");
-
-
-   while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
-        for (int n=1 ; n<2 ; n++)
-        {
-    inputletterint=ch-96;
-    newLetterInteger=((inputletterint+k));
-    newLetter=newLetterInteger+96;
-    if(newLetter<123 || newLetter>96 || newLetter==32){
-        if (newLetter>122){
-            newLetter=newLetter-26;
-        }
-        if (newLetter<97){
-            newLetter=newLetter+26;
-        }
-        if (newLetter==76){
-            newLetter=32;
-        }
-    }
-    if (newLetter>123 || newLetter<96){
-        newLetter=ch;
-    }
-
+    k1=12;
+      FILE *inputtext;
+    inputtext=fopen("input.txt", "r");
+    char ch;
+    char newLetter;
+    while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+    {
+    newLetter=subCyph(ch);
     printf("%c", newLetter);
     }
-    
-    fclose(inputtext);
+
 }
 
 
 char subCyph(char inputLetter)
 {
-    inputLetter=ch-96;
-    inputLetter=((inputletter+k));
-    inputLetter=inputLetter+96;
-    if(inputLetter<123 || inputLetter>96 || inputLetter==32){
-        if (inputLetter>122){
-            inputLetter=inputLetter-26;
+    char newLetters;
+    newLetters=inputLetter+k1;
+    if(inputLetter<91 && inputLetter>64){
+        if (newLetters>90){
+            newLetters=newLetters-26;//could be 26
         }
-        if (inputLetter<97){
-            inputLetter=inputLetter+26;
-        }
-        if (inputLetter==76){
-            inputLetter=32;
+        if (newLetters<63){
+            newLetters=newLetters+26;//could be 26
         }
     }
-    if (inputLetter>123 || inputLetter<96){
-        inputLetter=ch;
+    else
+    {
+        newLetters=inputLetter;
     }
-
-    printf("%c", inputLetter);
-    }
-    
-    fclose(inputtext);
+    return newLetters;
 }
