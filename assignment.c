5102b37806cd67da7bcdcab966d9a5d37c941f7a
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-23 04:17:46 +0000

    finally finished rotation cypher, began making substitution function

--- assignment.c
+++ assignment.c
@@ -1,35 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-int k1;
+int k1=1;
+char rotCyph(char inputLetter);
 char subCyph(char inputLetter);
 
 int main()
 {
     int choice;
-    printf("Welcome, choose either:\n1.Rotation Cypher Encryption\n2.Rotation Cypher Decryption\n3.Substitution Cypher Encryption\n4.Substitution Cypher Decryption\n\n");
+    printf("Welcome, type message to be encrypted/decrypted into input.txt, then, choose either:\n1.Rotation Cypher Encryption\n2.Rotation Cypher Decryption\n3.Substitution Cypher Encryption\n4.Substitution Cypher Decryption\n\n");
     scanf("%d", &choice);
     if (choice==1)
     {
-        printf("Enter a key");
-        scanf("%d", &k);
-    }
-    
-    
-      FILE *inputtext;
-    inputtext=fopen("input.txt", "r");
-    char ch;
-    char newLetter;
-    while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        printf("Enter a key (integer between [0,25])\n");
+        scanf("%d", &k1);
+        
+              FILE *inputtext;
+        inputtext=fopen("input.txt", "r");
+        char ch;
+        char newLetter;
+        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        {
+            newLetter=rotCyph(ch);
+            printf("%c", newLetter);
+        }
+        printf("\n");
+        }
+        
+    if (choice==2)
     {
-    newLetter=subCyph(ch);
-    printf("%c", newLetter);
-    }
-    printf("\n");
+        printf("Enter assigned key\n");
+        scanf("%d", &k1);
+        k1=-k1;
+        
+              FILE *inputtext;
+        inputtext=fopen("input.txt", "r");
+        char ch;
+        char newLetter;
+        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        {
+            newLetter=rotCyph(ch);
+            printf("%c", newLetter);
+        }
+        printf("\n");
+        }
 
 }
 
 
-char subCyph(char inputLetter)
+char rotCyph(char inputLetter)
 {
     char newLetters;
     newLetters=inputLetter+k1;
@@ -46,4 +64,9 @@ char subCyph(char inputLetter)
         newLetters=inputLetter;
     }
     return newLetters;
+}
+
+char subCyph(char inputLetter)
+{
+    
 }
