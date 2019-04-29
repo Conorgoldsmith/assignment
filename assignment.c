ac971193e89e4b07091f7aef8627dac848b90e19
Conor Goldsmith <conordgoldsmith@gmail.com>
2019-04-29 05:50:01 +0000

    working, adding comments

--- assignment.c
+++ assignment.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int key2;
-int key1;
-int k1=1;
-int k2;
+//using the header for function prototypes and global variables
+
+int key2; //initialising key2 for most common letter T as a global variable 
+int key1; //initialising key1 for most common letter E as global variable
+int k1=1; //initialising key for rotational cypher as global variable
+int k2; //initialising key for rotational cypher decryption as global variable
+
+//these next lines are the function prototypes for each function called within main
 char rotCyph(char inputLetter);
 char rotCyphUnk(char inputLetter);
 char rotCyphUnk1(char inputLetter);
@@ -12,21 +16,25 @@ char subCyph(char inputLetter);
 char subCyphDec(char inputLetter);
 char subKey[128];
 
+//end of header, start of the main function
+
 int main()
 {
-    int choice;
+    int choice; // initialises a variable to take input from the user for interface selection
+    //prints the user interface
     printf("Welcome, type message to be encrypted/decrypted into input.txt, then, choose either:\n1.Rotation Cypher Encryption\n2.Rotation Cypher Decryption\n3.Substitution Cypher Encryption\n4.Substitution Cypher Decryption\n5.Rotation Cypher Decryption Without Key\n\n");
-    scanf("%d", &choice);
-    if (choice==1)
+    scanf("%d", &choice);//scans users input and stores in choice
+    if (choice==1) 
     {
-        printf("Enter a key (integer between [0,25])\n");
-        scanf("%d", &k1);
+        //if the user enters 1 this code is executed
+        printf("Enter a key (integer between [0,25])\n"); //prints instructions for user
+        scanf("%d", &k1); //reads users input for the key
         
-              FILE *inputtext;
-        inputtext=fopen("input.txt", "r");
+              FILE *inputtext; 
+        inputtext=fopen("input.txt", "r");//initialises, opens and reads the file input.txt
         char ch;
         char newLetter;
-        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        while((ch = fgetc(inputtext)) != EOF) //runs through the file input.txt character by character
         {
             newLetter=rotCyph(ch);
             printf("%c", newLetter);
@@ -44,7 +52,7 @@ int main()
         inputtext=fopen("input.txt", "r");
         char ch;
         char newLetter;
-        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        while((ch = fgetc(inputtext)) != EOF) //runs through the file input.txt character by character
         {
             newLetter=rotCyph(ch);
             printf("%c", newLetter);
@@ -63,12 +71,12 @@ int main()
             FILE *subkey;
         subkey=fopen("substitutionkey.txt", "r");
 
-        while((keyletter = fgetc(subkey)) != EOF) // channge ch to another name 
+        while((keyletter = fgetc(subkey)) != EOF) //runs through the file input.txt character by character 
         {
             subKey[n]=keyletter;
             n++;
         }
-        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        while((ch = fgetc(inputtext)) != EOF) //runs through the file input.txt character by character 
         {
             newLetter=ch;
             if(ch<123 && ch>63)
@@ -92,12 +100,12 @@ int main()
             FILE *subkey;
         subkey=fopen("substitutionkey.txt", "r");
 
-        while((keyletter = fgetc(subkey)) != EOF) // channge ch to another name 
+        while((keyletter = fgetc(subkey)) != EOF) //runs through the file input.txt character by character
         {
             subKey[n]=keyletter;
             n++;
         }
-        while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
+        while((ch = fgetc(inputtext)) != EOF) //runs through the file input.txt character by character 
         {
             newLetter=ch;
             if(ch<123 && ch>63)
@@ -129,17 +137,16 @@ int main()
         float max=-1e30;
         int maxindex;
         char inputArray[10000];
-        char newLetters;
         char newL, newL1;
         int cntA, cntB, cntC, cntD, cntE, cntF, cntG, cntH, cntI, cntJ, cntK, cntL, cntM, cntN, cntO, cntP, cntQ, cntR, cntS, cntT, cntU, cntV, cntW, cntX, cntY, cntZ;
         while((ch = fgetc(inputtext)) != EOF) // channge ch to another name 
         {
-            inputArray[n]=ch;
+            inputArray[n]=ch; //takes the input and puts it into an array
             ++n;
         }
-        for(i=1;i<128;i++)
+        for(i=1;i<1000;i++)//counts through the input 
         {
-            switch(inputArray[i])
+            switch(inputArray[i]) //counts the frequency of each letter in the file to find which letter is the most common letter
             {
                 case 65:cntA++;
                     break;
@@ -197,6 +204,7 @@ int main()
                     break;
             }
         }
+        //initialising data to store the frequency of the letters within the input file 
     int data[26];
     data[0]=cntA; data[1]=cntB; data[2]=cntC; data[3]=cntD; data[4]=cntE; data[5]=cntF; data[6]=cntG;
     data[7]=cntH; data[8]=cntI; data[9]=cntJ; data[10]=cntK; data[11]=cntL; data[12]=cntM; data[13]=cntN;
@@ -206,12 +214,12 @@ int main()
     {
         if(data[count]>max)
         {
-            max=data[count];
+            max=data[count];//finds the most common letter 
             maxindex=count;
         }
         count++;
     }
-    
+    //determines the key if the most common letter is E
         if(maxindex-4>0)
         {
             key1=maxindex+65-69;
@@ -224,7 +232,7 @@ int main()
         {   
             key1=maxindex+65;
         }
-    
+    //determines the key if the most common letter is E
         if(maxindex-15>0)
         {
             key2=maxindex+65-84;
@@ -238,24 +246,30 @@ int main()
         key2=maxindex+65;
     }
     printf("If E is the most common letter the message is:\n");
-    while((ch1 = fgetc(inputtext1)) != EOF) // channge ch to another name 
+    while((ch1 = fgetc(inputtext1)) != EOF) // goes through the file character by character 
         {
-            newL=rotCyphUnk(ch1);
-            printf("%c", newL);
+            newL=rotCyphUnk(ch1); //uses the rotation cphypher function to decode each letter
+            printf("%c", newL); //prints the new letter
         }
     
     printf("\n");
     printf("If T is the most common letter the message is:\n");
-        while((ch2 = fgetc(inputtext2)) != EOF) // channge ch to another name 
+        while((ch2 = fgetc(inputtext2)) != EOF) // goes through the file character by character 
         {
-            newL1=rotCyphUnk1(ch2);
-            printf("%c", newL1);
+            newL1=rotCyphUnk1(ch2);//uses the rotation cphypher function to decode each letter
+            printf("%c", newL1);//prints the new letter
         }
     printf("\n");
+    }
+    // if the choice is not one of the options these lines of code give an error message
+    if(choice!=1 || choice!=2 || choice!=3 || choice!=4 || choice!=5)
+    {
+        printf("Restart the program and enter a number between 1 and 5");
     }
     return 0;
 }
 
+//these are where all of the functions are defined, under main
 
 char rotCyph(char inputLetter)
 {
